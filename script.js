f14ef82919f7a17e7232491ee88c32044d4eee29
Natan Guimarães <107555990+nacaxi@users.noreply.github.com>
2024-11-19 07:53:42 -0300

    a

--- script.js
+++ script.js
@@ -105,7 +105,7 @@ function respostaSelecionada(opcaoSelecionada) {
 }
 
 function mostraResultado() {
-    caixaPerguntas.textContent = "Em 2049...";
+    caixaPerguntas.textContent = "Em Harry Potter, você seria...";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
 }
